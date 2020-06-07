87078d392e67dc7922791e47b96cba7efec0c5b7
Devang Jhabakh Jai <devangjhabakh@berkeley.edu>
2020-06-07 23:38:30 +0400

    Added the ability to sort the heap through a new constructor. Read comments.

--- maxheaps.h
+++ maxheaps.h
@@ -6,12 +6,62 @@ template <typename Item>
 //IMPLEMENTATION NOTE:
 //Whenever you do use this with a user-defined class, be sure that the class that you use this with has 
 //defined methods for operator>() AND operator<(), as they are both used here.
+//To heap sort an array, convert it into a valid heap using the Parameterized Heap constructor. Then, 
+//repetedly extract from the heap in order to get all the elements in a sorted manner.
 class Heap{
     Item HeapArray[HEAPSIZE];
     int count;
     public:
     Heap(){
         count = 0;
+    }
+    Heap(Item InitialArray[], int GivenCount){        //Take a given array and turn it into a valid heap. (Defined in Notes under Heap Sort)
+        for (int i=0;i<GivenCount;++i){
+            HeapArray[i] = InitialArray[i];
+        }
+        count = GivenCount;
+        int CurrNode = this -> GetParentAddress(GivenCount - 1);
+        while (CurrNode != -1){
+            int TraversalNode = CurrNode;
+            int RightChildAddress = this -> GetRightChild(TraversalNode);
+            int LeftChildAddress = this -> GetLeftChild(TraversalNode);
+            Item RightChild = HeapArray[RightChildAddress];
+            Item LeftChild = HeapArray[LeftChildAddress];
+            Item temp;
+            while (LeftChildAddress <= GivenCount - 1){
+                if (RightChildAddress>=count){
+                    if(LeftChild > HeapArray[TraversalNode]){
+                        temp = LeftChild;
+                        HeapArray[LeftChildAddress] = HeapArray[TraversalNode];
+                        HeapArray[TraversalNode] = temp;
+                        TraversalNode = LeftChildAddress;
+                    }
+                    break;
+                }
+                else if (RightChild > HeapArray[TraversalNode] || LeftChild > HeapArray[TraversalNode]){
+                    if(RightChild>LeftChild){
+                        temp = RightChild;
+                        HeapArray[RightChildAddress] = HeapArray[TraversalNode];
+                        HeapArray[TraversalNode] = temp;
+                        TraversalNode = RightChildAddress;
+                    }
+                    else{
+                        temp = LeftChild;
+                        HeapArray[LeftChildAddress] = HeapArray[TraversalNode];
+                        HeapArray[TraversalNode] = temp;
+                        TraversalNode = LeftChildAddress;
+                    }
+                }
+                else{
+                    break;
+                }
+                RightChildAddress = this -> GetRightChild(TraversalNode);
+                LeftChildAddress = this -> GetLeftChild(TraversalNode);
+                RightChild = HeapArray[RightChildAddress];                 
+                LeftChild = HeapArray[LeftChildAddress];
+            }
+            CurrNode--;
+        }
     }
     int GetSizeOfHeap(){
         return count;
