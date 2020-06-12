18346b8884d67e2ad662c9c598ecbf22f4a7685a
Devang Jhabakh Jai <devangjhabakh@berkeley.edu>
2020-06-13 00:26:40 +0400

    Added documentation and slightly generalised code

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#define NUM_VERTICES 9
 using namespace std;
 bool CheckIfAllTrue(bool *boolArray, int n){
     for (int i = 0; i < n; ++i){
@@ -11,7 +12,7 @@ bool CheckIfAllTrue(bool *boolArray, int n){
     return true;
 }
 
-void AdjacentMinDistance(int G1[][9], bool Done[], int n, int start,int pathLengths[], int MinIndexPath[]){
+void AdjacentMinDistance(int G1[][NUM_VERTICES], bool Done[], int n, int start,int pathLengths[], int MinIndexPath[]){
     int minpath = INT16_MAX, minindex = 0;
     for (int i=0;i<n;++i){
         if(G1[start][i]<minpath && G1[start][i] != 0 && Done[i] == false){  //Initialize the distances for all adjacent vertices.
@@ -20,7 +21,7 @@ void AdjacentMinDistance(int G1[][9], bool Done[], int n, int start,int pathLeng
             }
         }
     }
-    for (int i=0;i<n;++i){                                                  //Find the minimum distance out of ALL vertices.
+    for (int i=0;i<n;++i){                                                  //Find the minimum distance out of ALL vertices. 
         if(Done[i] == false){
             if(pathLengths[i] < minpath){
                  minpath = pathLengths[i];
@@ -32,21 +33,21 @@ void AdjacentMinDistance(int G1[][9], bool Done[], int n, int start,int pathLeng
     MinIndexPath[1] = minpath;
 }
 
-void Djikstra(int G1[][9], int n, int start){
-    bool Done[9];
-    int pathLengths[9];
+void Dijkstra(int G1[][NUM_VERTICES], int n, int start){
+    bool Done[NUM_VERTICES];                                //A boolean array that keeps track of whether vertex labeled "i" had its shortest path discovered.
+    int pathLengths[NUM_VERTICES];                          //An integer array that keeps track of the minimum distances from our starting vertex to any given vertex.
     for(int i = 0;i<n;++i){
         Done[i] = false;
         pathLengths[i] = INT16_MAX;
     }
-    Done[start] = true;
+    Done[start] = true;                             //Initialising special values for the starting node.
     pathLengths[start] = 0;
     int minpath = INT16_MAX, minindex = 0;
     while(!CheckIfAllTrue(Done,n)){
-        int MinIndexPath[2];
-        AdjacentMinDistance(G1,Done,n,minindex,pathLengths,MinIndexPath);
-        Done[MinIndexPath[0]] = true;
-        minindex = MinIndexPath[0];
+        int MinIndexPath[2];                        //A temporary pair of values that keep track of the next minimum index and its path length for a given iteration of Dijkstra.
+        AdjacentMinDistance(G1,Done,n,minindex,pathLengths,MinIndexPath);   //This function initialises the minimum distance for all adjacent vertices.
+        Done[MinIndexPath[0]] = true;       //Mark the vertex with the current minimum path as done.
+        minindex = MinIndexPath[0];         
     }; 
     cout<<"The distances are: \n";
     for(int i=0;i<n;++i){
@@ -55,7 +56,7 @@ void Djikstra(int G1[][9], int n, int start){
 }
 
 int main(){ 
-    int graph[9][9] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 }, 
+    int graph[NUM_VERTICES][NUM_VERTICES] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 }, 
                         { 4, 0, 8, 0, 0, 0, 0, 11, 0 }, 
                         { 0, 8, 0, 7, 0, 4, 0, 0, 2 }, 
                         { 0, 0, 7, 0, 9, 14, 0, 0, 0 }, 
@@ -64,7 +65,7 @@ int main(){
                         { 0, 0, 0, 0, 0, 2, 0, 1, 6 }, 
                         { 8, 11, 0, 0, 0, 0, 1, 0, 7 }, 
                         { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
-    Djikstra(graph,9,0);
+    Dijkstra(graph,NUM_VERTICES,0);
     cout<<"End of program!";
     return 1;
 }
